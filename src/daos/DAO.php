0c600f66dbebe26a4050a4d490af5a1655ad4d31
Khattabi Oussama <la216646@student.helha.be>
2023-03-30 13:41:02 +0200

    fix: fixing params

--- src/daos/DAO.php
+++ src/daos/DAO.php
@@ -4,10 +4,11 @@ namespace CashRegister\daos;
 
 interface DAO
 {
+
     public function insert(object $insertedObject): bool;
-    public function selectOne(object $selectedObject): object;
-    public function selectAll(object $selectedObjects): array;
-    public function selectWhere(object $selectedObjects): array;
+    public function selectOne(int $id): object;
+    public function selectAll(): array;
+    public function selectWhere(array $params): array;
     public function update(object $objectToUpdate): bool;
     public function delete(object $objectToDelete): bool;
 }
