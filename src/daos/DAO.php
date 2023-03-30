e3574fccbf60da8d2c8a9fbb6c271ac11ebd6004
Khattabi Oussama <la216646@student.helha.be>
2023-03-30 12:13:51 +0200

    fix: any don't exist, replaced by object

--- src/daos/DAO.php
+++ src/daos/DAO.php
@@ -5,7 +5,7 @@ namespace CashRegister\daos;
 interface DAO
 {
     public function insert(string $table, array $params): bool;
-    public function selectOne(string $table, int $id): any;
+    public function selectOne(string $table, int $id): object;
     public function selectAll(string $table): array;
     public function selectWhere(string $table, array $params): array;
     public function update(string $table, array $params): bool;
