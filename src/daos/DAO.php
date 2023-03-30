b01239e668ebfff4faf9a5063806cc0d0e8fb0fc
Khattabi Oussama <la216646@student.helha.be>
2023-03-30 12:47:49 +0200

    fix: passing object attribute in methods

--- src/daos/DAO.php
+++ src/daos/DAO.php
@@ -4,10 +4,10 @@ namespace CashRegister\daos;
 
 interface DAO
 {
-    public function insert(string $table, array $params): bool;
-    public function selectOne(string $table, int $id): object;
-    public function selectAll(string $table): array;
-    public function selectWhere(string $table, array $params): array;
-    public function update(string $table, array $params): bool;
-    public function delete(string $table, array $params): bool;
+    public function insert(object $insertedObject): bool;
+    public function selectOne(object $selectedObject): object;
+    public function selectAll(object $selectedObjects): array;
+    public function selectWhere(object $selectedObjects): array;
+    public function update(object $objectToUpdate): bool;
+    public function delete(object $objectToDelete): bool;
 }
