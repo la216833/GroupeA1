99ac061cde03bddf6ceb1044f44d39a0dca8414e
Khattabi Oussama <la216646@student.helha.be>
2023-05-05 21:20:01 +0200

    feat: Login view

---
+++ src/views/login.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/styles/styles.css">
+    <title>Login - getEntityName</title>
+</head>
+
+<body class="login-body">
+<div>
+    <img src="https://logowiki.net/wp-content/uploads/imgp/Test-Logo-1-2569.png" alt="" >
+</div>
+<div>
+    <form class="login-form-container" action="">
+        <input class="login-input" type="password" minlength="6" maxlength="6" autocomplete="off" required inputmode="numeric" pattern="^[0-9]{1,6}$" autofocus>
+        <button class="btn login-btn" type="submit">Se connecter</button>
+    </form>
+</div>
+</body>
+</html>
+
+
+
