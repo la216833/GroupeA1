035368aedf13cbdea47d371479c66d04ef3c1d67
Dumont Logan <la216833@student.helha.be>
2023-05-10 23:14:14 +0200

    feat: CategoryController get page

--- src/views/categories.php
+++ src/views/categories.php
@@ -1,19 +1,19 @@
 <div class="container">
     <div class="stats">
         <div class="stat stat-info">
-            <h2 class="stat-title">35</h2>
+            <h2 class="stat-title"><?= count($params['categories'])?></h2>
             <p class="stat-desc">Nombre de catégories</p>
         </div>
         <div class="stat stat-success">
-            <h2 class="stat-title">29</h2>
+            <h2 class="stat-title"><?= count($params['category_active'])?></h2>
             <p class="stat-desc">Catégories actives</p>
         </div>
         <div class="stat stat-warning">
-            <h2 class="stat-title">4</h2>
+            <h2 class="stat-title"><?= count($params['category_empty'])?></h2>
             <p class="stat-desc">Catégories vides</p>
         </div>
         <div class="stat stat-danger">
-            <h2 class="stat-title">1</h2>
+            <h2 class="stat-title"><?= count($params['category_inactive'])?></h2>
             <p class="stat-desc">Catégories inactives</p>
         </div>
     </div>
@@ -30,16 +30,20 @@
             </tr>
             </thead>
             <tbody>
+            <?php foreach ($params['categories'] as $category): ?>
             <tr>
-                <td class="hide">1</td>
-                <td class="list-large">Boissons</td>
-                <td class="table-right status status-success">Actif</td>
-                <td class="table-right">2</td>
+                <td class="hide"><?= $category->getID()?></td>
+                <td class="list-large"><?= $category->getName()?></td>
+                <td class="table-right status status-success <?= $category->getActive() ? 'status-success': 'status-danger'?>">
+                    <?= $category->getActive() ? 'actif': 'Inactif'?>
+                </td>
+                <td class="table-right">36</td>
                 <td class="table-right">
-                    <a class="btn btn-action btn-info" href="/category/:id">Modifier</a>
-                    <a class="btn btn-action btn-red" href="/category-del/:id">Supprimer</a>
+                    <a class="btn btn-action btn-info" href="/category/<?= $category->getID()?>">Modifier</a>
+                    <a class="btn btn-action btn-red" href="/category-del/<?= $category->getID()?>">Supprimer</a>
                 </td>
             </tr>
+            <?php endforeach; ?>
             </tbody>
         </table>
     </div>
