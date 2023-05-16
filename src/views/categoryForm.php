1bba6157e3810260f0e54a29335f5ce87edc7d77
la215871 Bilal K <la215871@student.helha.be>
2023-05-16 17:34:01 +0200

    fix: url

--- src/views/categoryForm.php
+++ src/views/categoryForm.php
@@ -4,23 +4,23 @@
     <form method="post" class="form category-container">
         <div class="form-group">
             <label for="">Nom
-                <input type="text" name="name" value="<?= empty($params) ?: $params['category']->getName() ?>">
+                <input type="text" name="name" value="<?= empty($params) ? '': $params['category']->getName() ?>">
             </label>
         </div>
         <div class="form-group">
             <label for="">Description
-                <input type="text" value="<?= empty($params) ?: $params['category']->getDescription() ?>">
+                <input type="text" value="<?= empty($params) ? '': $params['category']->getDescription() ?>">
             </label>
         </div>
         <div class="form-group">
-            <label for="">Statut</label>
-            <select name="status">
-                <option value="">Actif</option>
-                <option value="">Non actif</option>
-            </select>
+            <label for="status">Statut
+                <select name="status">
+                    <option value="1">ACTIF</option>
+                    <option value="0">NON ACTIF</option>
+                </select>
+            </label>
         </div>
         <input type="submit" value="Valider" class="btn btn-dark btn-md">
-        <button class="btn btn-md btn-dark" onclick="location.href='/categories'">Annuler</button>
     </form>
-
+    <button class="btn btn-md btn-dark" onclick="location.href='/categories'">Annuler</button>
 </div>
