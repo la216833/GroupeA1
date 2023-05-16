7f0e41a8ad1c43e187e00938762d774cf8eff2e6
la215871 Bilal K <la215871@student.helha.be>
2023-05-16 17:31:30 +0200

    refact: remove methods params

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -6,7 +6,7 @@ interface Controller {
     public function get(): void;
     public function get_one(int $id): void;
     public function add(): void;
-    public function post(array $params):void;
+    public function post():void;
     public function post_one(int $id): void;
     public function update(int $id): void;
     public function delete(int $id): void;
