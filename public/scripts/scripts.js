0d87c6e2fa647bc839f089f1ed9e36572e4e79d5
la215871 Bilal K <la215871@student.helha.be>
2023-05-26 18:16:37 +0200

    docs: js script documentation

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,17 +1,127 @@
-const btn = document.getElementById('showMore');
-const plusContent = document.getElementById('plusContent')
-const products = document.getElementById('products');
-const table = document.getElementById('table');
-const totalText = document.getElementById('total');
+const btn = document.getElementById('showMore');            // Btn to show more options on ticket
+const plusContent = document.getElementById('plusContent'); // Additional command related to btn
+const products = document.getElementById('products');       // Products in the grid
+const table = document.getElementById('table');             // Ticket table
+const totalText = document.getElementById('total');         // Text to modify current amount
+const sidebar = document.getElementById('sidebar');         // Sidebar menu content
+const sidebarBtn = document.getElementById('sidebarBtn');   // Sidebar button to show menu
+const navBtns = document.getElementById('nav');             // Category navigation on sale products
+const catChoice = document.getElementById('catChoice');     // Select input to chose category
+const userChoice = document.getElementById('userChoice');   // Select input to chose role
 
-const sidebar = document.getElementById('sidebar');
-const sidebarBtn = document.getElementById('sidebarBtn');
+/*
+* Show or hide content
+*
+* Event Listener that hides and shows more content
+*
+* */
+if (btn != null) {
+    btn.addEventListener('click', () => {
+        plusContent.classList.toggle('show');
+        btn.innerHTML = btn.innerHTML === "+" ? "-": "+";
+    })
+}
 
-const navBtns = document.getElementById('nav');
+/*
+*
+*  Add product to cart
+*
+* */
+if (products != null) {
+    products.addEventListener('click', event => {
+        let card = event.target.closest('.card');
+        addToCart(card)
+        total();
+    })
+}
 
-const catChoice = document.getElementById('catChoice')
-const userChoice = document.getElementById('userChoice')
+/*
+*
+*  Delete or remove one by one product from cart
+*
+* */
+if (table != null) {
+    table.addEventListener('click', event => {
+        if(event.target.classList.value === 'table-delete') {
+            const row = event.target.closest('tr')
+            let quantity = parseInt(row.childNodes[2].innerHTML)
+            let price = parseFloat(row.childNodes[3].innerHTML)
+            if (quantity === 1) {
+                table.removeChild(row)
+            }
 
+            row.childNodes[2].innerHTML = (quantity - 1)
+            row.childNodes[6].childNodes[0].value -= 1;
+            row.childNodes[3].innerHTML = (price - price/quantity).toFixed(2)
+            total();
+        }
+
+    })
+}
+
+/*
+* Show or hide sidebar menu
+*
+* Event Listener that hides and shows sidebar menu on click event
+*
+* */
+if (sidebarBtn != null) {
+    sidebarBtn.addEventListener('click', () => {
+        sidebarBtn.classList.toggle('enable');
+        sidebar.classList.toggle('show');
+        sidebarBtn.innerHTML = sidebarBtn.classList.contains('enable') ? "<": ">";
+
+    })
+}
+
+
+/*
+* Load products by category in sale view
+*
+* Event Listener that hides and shows products in grid
+* depending on the category selected
+*
+* */
+if (navBtns != null) {
+    navBtns.addEventListener('click', event => {
+        if(event.target.classList.value.includes('btn')) {
+            const cat = event.target.innerHTML;
+            products.childNodes.forEach(p => {
+                if (p.nodeName === 'DIV') {
+                    if (event.target.getAttribute('aria-details') === 'all') {
+                        p.style.display = 'block';
+                        return;
+                    }
+                    if (cat !== p.getAttribute('aria-details')) {
+                        p.style.display = 'none';
+                    } else {
+                        p.style.display = 'block'
+                    }
+                }
+            })
+
+            navBtns.childNodes.forEach(btn => {
+                if (btn.nodeName === 'BUTTON') {
+                    if (cat === btn.innerHTML) {
+                        if (btn.classList.value.includes('btn-light')) btn.classList.remove('btn-light')
+                        if (!btn.classList.value.includes('btn-active')) btn.classList.add('btn-active')
+                    } else {
+                        if (!btn.classList.value.includes('btn-light')) btn.classList.add('btn-light')
+                        if (btn.classList.value.includes('btn-active')) btn.classList.remove('btn-active')
+                    }
+                }
+            })
+        }
+    })
+}
+
+/*
+* Load products by category
+*
+* Event Listener that hides and shows products in list (table tag)
+* depending on the category selected
+*
+* */
 if (catChoice != null) {
     catChoice.addEventListener('change', () => {
         const cat = catChoice.value
@@ -32,6 +142,13 @@ if (catChoice != null) {
     })
 }
 
+/*
+* Load users by role
+*
+* Event Listener that hides and shows users in list (table tag)
+* depending on the role selected
+*
+* */
 if (userChoice != null) {
     userChoice.addEventListener('change', () => {
         const type = userChoice.value
@@ -52,83 +169,13 @@ if (userChoice != null) {
     })
 }
 
-if (navBtns != null) {
-    navBtns.addEventListener('click', event => {
-        if(event.target.classList.value.includes('btn')) {
-            const cat = event.target.innerHTML;
-            products.childNodes.forEach(p => {
-                if (p.nodeName === 'DIV') {
-                    if (event.target.getAttribute('aria-details') === 'all') {
-                        p.style.display = 'block';
-                        return;
-                    }
-                    if (cat !== p.getAttribute('aria-details')) {
-                        p.style.display = 'none';
-                    } else {
-                        p.style.display = 'block'
-                    }
-                }
-            })
-            navBtns.childNodes.forEach(btn => {
-                if (btn.nodeName === 'BUTTON') {
-                    if (cat === btn.innerHTML) {
-                        if (btn.classList.value.includes('btn-light')) btn.classList.remove('btn-light')
-                        if (!btn.classList.value.includes('btn-active')) btn.classList.add('btn-active')
-                    } else {
-                        if (!btn.classList.value.includes('btn-light')) btn.classList.add('btn-light')
-                        if (btn.classList.value.includes('btn-active')) btn.classList.remove('btn-active')
-                    }
-                }
-            })
-        }
-
-    })
-}
-
-if (sidebarBtn != null) {
-    sidebarBtn.addEventListener('click', () => {
-        sidebarBtn.classList.toggle('enable');
-        sidebar.classList.toggle('show');
-        sidebarBtn.innerHTML = sidebarBtn.classList.contains('enable') ? "<": ">";
-
-    })
-}
-
-if (btn != null) {
-    btn.addEventListener('click', () => {
-        plusContent.classList.toggle('show');
-        btn.innerHTML = btn.innerHTML === "+" ? "-": "+";
-    })
-}
-
-if (products != null) {
-    products.addEventListener('click', event => {
-        let card = event.target.closest('.card');
-        addToCart(card)
-        total();
-    })
-}
-
-
-if (table != null) {
-    table.addEventListener('click', event => {
-        if(event.target.classList.value === 'table-delete') {
-            const row = event.target.closest('tr')
-            let quantity = parseInt(row.childNodes[2].innerHTML)
-            let price = parseFloat(row.childNodes[3].innerHTML)
-            if (quantity === 1) {
-                table.removeChild(row)
-            }
-
-            row.childNodes[2].innerHTML = (quantity - 1)
-            row.childNodes[6].childNodes[0].value -= 1;
-            row.childNodes[3].innerHTML = (price - price/quantity).toFixed(2)
-            total();
-        }
-
-    })
-}
-
+/*
+* Add to cart
+*
+* Add product to cart or increase quantity it is
+* already in cart
+*
+* */
 function addToCart(card) {
     let exist = false;
     let quantity = 1;
@@ -152,6 +199,7 @@ function addToCart(card) {
         }
     }
 
+    // HTML generating
     if (!exist) {
         let row = table.insertRow(-1);
 
@@ -183,6 +231,12 @@ function addToCart(card) {
     }
 }
 
+
+/*
+*
+*  Get the sum of all products
+*
+* */
 function total() {
     let priceTot = 0;
     for (let i = 0; i < table.childNodes.length; i++) {
