55849a599b2a4e2e75e60c34345a0ae066acc689
la215871 Bilal K <la215871@student.helha.be>
2023-05-29 10:45:15 +0200

    refact: users default password

--- migrations/m0009_users.php
+++ migrations/m0009_users.php
@@ -7,11 +7,11 @@ class m0009_users {
     public function up(): void {
         $db = DBConnection::getInstance();
         $query = "INSERT INTO users (usersName, usersFirstname, usersAccessCode, usersImagePath, rolesID) VALUES 
-            ('Jobin', 'Alfred', '39e0274dd5962cf0586fdde1b1108de1c3b3f19c', 'default-user-icon.png', 3),
-            ('Lacasse', 'Charlot', '28fb1e741c3a51f3ade6aa2ef44020987dff0438', 'default-user-icon.png', 2),
-            ('Brasseur', 'Denis', '28fb1e741c3a51f3ade6aa2ef44020987dff0438', 'default-user-icon.png', 2),
-            ('Thériault', 'Raina', '28fb1e741c3a51f3ade6aa2ef44020987dff0438', 'default-user-icon.png', 1),
-            ('Flamand', 'Sébastien', '28fb1e741c3a51f3ade6aa2ef44020987dff0438', 'default-user-icon.png', 1);
+            ('Jobin', 'Alfred', '$2y$10\$m9EyT0.BvgJ31xSuUbigXuJMtCT5ev7XoyB4PNgVPiWYXwQAYpWS6', 'default-user-icon.png', 3),
+            ('Lacasse', 'Charlot', '$2y$10\$t48cmNWHUCuyR8DU7ab3lus6H/ti7dJs2m0awSNBI/oaAcu1EyZWu', 'default-user-icon.png', 2),
+            ('Brasseur', 'Denis', '$2y$10\$pc64Wa.DhrcPs1KQVXKS9.pUhuEkvNLbWnoCb.IBrt7.N70Mazj4m', 'default-user-icon.png', 2),
+            ('Thériault', 'Raina', '$2y$10\$JsCaOJYFVm1lqDt9Sgys/eqMHdxWdxdBycT/5AAls3rgrQSLg410y', 'default-user-icon.png', 1),
+            ('Flamand', 'Sébastien', '$2y$10\$wGqA/e3IBGu6F8nrsXlGOO7UEebeaqSnpfDTaTPiy/Ligufg1lrpG', 'default-user-icon.png', 1);
         ";
 
         $db->exec($query);
