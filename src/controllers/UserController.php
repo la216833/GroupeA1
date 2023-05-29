07e7277fe0a69075b439616171e354ecf9e7b291
Dumont Logan <la216833@student.helha.be>
2023-05-29 14:26:59 +0200

    Feat: add user controller

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -2,48 +2,168 @@
 
 namespace CashRegister\controllers;
 
+use CashRegister\core\exception\DBException;
 use CashRegister\daos\DAORole;
 use CashRegister\daos\DAOUser;
 use CashRegister\core\View;
+use CashRegister\models\User;
 
 class UserController implements Controller
 {
 
+
     private View $view;
+    private DAORole $DAORole;
+    private DAOUser $DAOUser;
 
     public function __construct() {
         $this->view = new View();
+        $this->DAOUser = new DAOUser();
+        $this->DAORole = new DAORole();
     }
     public function get(): void
     {
+        global $session;
+        if ($session->get('ROLE') !== 'administrator') {
+            header('Location: /');
+            exit();
+        }
         $params = [];
-        $userDao = new DAOUser();
-        $roleDao = new DAORole();
-        $params['roles'] = $roleDao->selectAll();
-        $params['users'] = $userDao->selectAll();
-        $params['users_seller'] = count($userDao->selectWhere(['rolesID' => 1]));
-        $params['users_manager'] = count($userDao->selectWhere(['rolesID' => 2]));
-        $params['users_admin'] = count($userDao->selectWhere(['rolesID' => 3]));
-        echo $this->view->render('users.php', $params);
+
+        try {
+            $params['roles'] = $this->DAORole->selectAll();
+            $params['users'] = $this->DAOUser->selectAll();
+            $params['users_seller'] = count($this->DAOUser->selectWhere(['rolesID' => 1]));
+            $params['users_manager'] = count($this->DAOUser->selectWhere(['rolesID' => 2]));
+            $params['users_admin'] = count($this->DAOUser->selectWhere(['rolesID' => 3]));
+        } catch (DBException) {
+            $params['errors'] = "Une erreur est survenue";
+        } finally {
+            echo $this->view->render('users.php', $params);
+        }
+
     }
 
     public function get_one(int $id): void
     {
+        global $session;
+        if ($session->get('ROLE') !== 'administrator') {
+            header('Location: /');
+            exit();
+        }
         $params = [];
-        $userDao = new DAOUser();
-        $params['user'] = $userDao->selectOne($id);
-        echo $this->view->render('userForm.php', $params);
+        try {
+            $params['user'] = $this->DAOUser->selectOne($id);
+        } catch (DBException) {
+            $params['errors'] = "Une erreur est survenue";
+        } finally {
+            echo $this->view->render('userForm.php', $params);
+        }
+
     }
 
     public function add(): void
     {
+        global $session;
+        if ($session->get('ROLE') !== 'administrator') {
+            header('Location: /');
+            exit();
+        }
         $params = [];
         echo $this->view->render('userForm.php', $params);
     }
 
     public function post(): void
     {
-        // TODO: Implement post() method.
+        global $session;
+        if ($session->get('ROLE') !== 'administrator') {
+            header('Location: /');
+            exit();
+        }
+        $params = [];
+        $data = $_POST;
+        if (isset($data)){
+            if (!empty($data['name']) &&
+                !empty($data['firstname']) &&
+                !empty($data['accesscode']) &&
+                !empty($data['role']) &&
+                !empty($data['status'])){
+
+                $name = htmlentities($data['name']);
+                $accessCode = htmlentities($data['accesscode']);
+                $role = htmlentities($data['role']);
+
+                    try {
+
+                        if(strlen($data['accessscode']) != 6 ||  !preg_match('/^\d+$/', $data['accesscode']))
+                            $params['errors']['accessscode'] = "Le code d'access doit contenir 6 caractères numériques";
+
+                        $accessCode = hash($accessCode,PASSWORD_DEFAULT);
+
+                        $users = $this->DAOUser->selectAll();
+                        foreach ($users as $user){
+                            if (password_verify($accessCode,$user->getAccessCode())){
+                                $currentUser = $user;
+                                break;
+                            }
+                        }
+
+                        if ($currentUser == null &&
+                            !empty($this->DAORole->selectWhere(["rolesName" => $role]))){
+
+                            if(strlen($name) < 3 || strlen($name) > 50)
+                                $params['errors']['name'] = "Le nom doit contenir entre 3 et 50 caractères";
+
+                            if(strlen($data['firstname']) < 3 || strlen($data['firstname']) > 50)
+                                $params['errors']['firstname'] = "Le prénom doit contenir entre 3 et 50 caractères";
+
+                            $status = (bool) $data['status'] ?? true;
+
+                            if (!is_array($params['errors'])){
+                                $user = new User(
+
+                                    0,
+                                    $name,
+                                    htmlentities($data['firstname']),
+                                    $data['accessscode'],
+                                    htmlentities($data['image']),
+                                    $status,
+                                    $this->DAORole->selectWhere(["rolesname" => $role])
+
+                                );
+
+                                if ($this->DAOUser->insert($user)){
+                                    header("Location: /users");
+                                    exit();
+                                }
+                            }
+
+                        }else {
+                            if (!$currentUser == null){
+                                $params['errors'] = "l'utilisateur existe dèja";
+                            } else if (empty($this->DAORole->selectWhere(["rolesName" => $role]))){
+                                $params['errors'] = "le role séléctionné n'existe pas";
+                            }
+                        }
+                    } catch (DBException){
+                        $params['errors'] = "Une erreur est survenue";
+                    }
+
+
+            }else {
+                $params['errors'] = "Tous les champs doivent être complete";
+            }
+        }
+        try {
+            $params['roles'] = $this->DAORole->selectAll();
+
+        }catch (DBException){
+            $params['errors'] = "Une erreur est survenue";
+        } finally {
+            $params['data'] = $data;
+            echo $this->view->render('userFrom.php', $params);
+        }
+
     }
 
     public function post_one(int $id): void
@@ -53,7 +173,59 @@ class UserController implements Controller
 
     public function update(int $id): void
     {
-        // TODO: Implement update() method.
+        global $session;
+        if ($session->get('ROLE') !== 'administrator') {
+            header('Location: /');
+            exit();
+        }
+        $params= array();
+        $data = $_POST;
+        try {
+            $user = $this->DAOUser->selectOne($id);
+            if($user && isset($data)){
+                $name = htmlentities($data['name']);
+                $firstname = htmlentities($data['firstname']);
+                $accesssCode = htmlentities($data['accessscode']);
+                $image = htmlentities($data['image']);
+                $role = htmlentities($data['role']);
+                $status = htmlentities($data['status']);
+
+                if (($name !== $user->getName()) && (strlen($name) > 3 && strlen($name) <= 50))
+                    $user->setName($name);
+                if (($firstname !== $user->getFirstname()) && (strlen($firstname) > 3 && strlen($firstname) <= 50))
+                    $user->setFirstname($firstname);
+                if (isset($accessCode)){
+                    if (strlen($accesssCode) != 6 ||  !preg_match('/^\d+$/', $accesssCode)){
+                        $accessCode = hash($accessCode,PASSWORD_DEFAULT);
+                        $users = $this->DAOUser->selectAll();
+                        foreach ($users as $user){
+                            if (password_verify($accessCode,$user->getAccessCode())){
+                                $currentUser = $user;
+                                break;
+                            }
+                        }
+                        if (!$currentUser == null){
+                            $params['errors'] = "l'utilisateur existe dèja";
+                        }else{
+                            $user->setAccessCode();
+                        }
+                    }
+                }
+                if ($role !== $user->getRole()){
+                    $user->setRole($role);
+                }
+
+                if ($status !== $user->getStatus()){
+                    $user->setStatus($status);
+                }
+                $this->DAOUser->update($user);
+
+            }
+        }catch (DBException){
+
+        } finally {
+            header('Location: /users');
+        }
     }
 
     public function delete(int $id): void
--- src/views/userForm.php
+++ src/views/userForm.php
@@ -1,40 +1,66 @@
+<?php if (isset($params['errors']) && !is_array($params['errors'])): ?>
+    <div class="alert-error"><?= $params['errors'] ?></div>
+<?php endif; ?>
 <div class="container center">
     <img src="https://armetiss.be/img/logo-active.png" alt="" class="add-img">
     <h2 class="title"><?= empty($params) ? 'Ajouter un utilisateur' : 'Modifier l\'utilisateur' ?></h2>
     <form method="post" class="form">
         <div class="form-group">
-            <label for="">Nom</label>
-            <input type="text" name="name" value="<?= empty($params) ?'': $params['user']->getName() ?>">
+            <label for="name">Nom</label>
+            <input type="text" name="name" value="<?=
+            empty($params['user']) ?'':
+                $params['user']->getName()
+            ?><?=
+            empty($params['data']) ? '':
+                $params['data']['name']?>">
+            <?php if (isset($params['errors']['name'])): ?>
+                <span class="input-error">*<?= $params['errors']['name'] ?></span>
+            <?php endif; ?>
         </div>
         <div class="form-group">
-            <label for="">Prénom</label>
-            <input type="text" value="<?= empty($params) ?'': $params['user']->getFirstName() ?>">
+            <label for="firstname">Prénom</label>
+            <input type="text" name="firstname" value="<?=
+            empty($params['user']) ?'':
+                $params['user']->getFirstName()
+            ?><?=
+            empty($params['data']) ? '':
+                $params['data']['firstname']?>">
+            <?php if (isset($params['errors']['firstname'])): ?>
+                <span class="input-error">*<?= $params['errors']['firstname'] ?></span>
+            <?php endif; ?>
         </div>
         <div class="form-group">
-            <label for="">Code d'accès</label>
-            <input type="password" minlength="6" maxlength="6" autocomplete="off" required inputmode="numeric" pattern="^[0-9]{1,6}$">
+            <label for="accesscode">Code d'accès</label>
+            <input type="password" name="accesscode" minlength="6" maxlength="6" autocomplete="off" required inputmode="numeric" pattern="^[0-9]{1,6}$">
+            <?php if (isset($params['errors']['accesscode'])): ?>
+                <span class="input-error">*<?= $params['errors']['accesscode'] ?></span>
+            <?php endif; ?>
         </div>
         <div class="form-group">
-            <label for="">Photo de profile</label>
-            <input type="file">
+            <label for="image">Photo de profile</label>
+            <input type="file" name="image">
         </div>
         <div class="form-group">
             <label for="">Role</label>
-            <select name="role">
-                <option value="0">Vendeur</option>
-                <option value="1">Responsable</option>
-                <option value="2">Administrateur</option>
-            </select>
+                <select name="role">
+                    <?php foreach ($params['roles'] as $role): ?>
+                        <option value="<?= $role->getName() ?>" <?= isset($params["user"]) && $role->getID() ==
+                        $params["user"]->getRole()
+                            ->getID()? 'selected="selected"' : ''
+                        ?>><?= $role->getName() ?></option>
+                    <?php endforeach; ?>
+                </select>
         </div>
         <div class="form-group">
             <label for="">Statut</label>
-            <select name="status">
-                <option value="1">ACTIF</option>
-                <option value="0">NON ACTIF</option>
-            </select>
+                <select name="status">
+                    <option value="0" <?= isset($params["user"]) && $params["user"]->getActive() == 0 ? 'selected="selected"' : ''
+                    ?>>NON ACTIF</option>
+                    <option value="1" <?= isset($params["user"]) && $params["user"]->getActive() == 1 ? 'selected="selected"' : ''
+                    ?>>ACTIF</option>
+                </select>
         </div>
         <input type="submit" value="Valider" class="btn btn-dark btn-md">
     </form>
     <button class="btn btn-md btn-dark" onclick="location.href='/users'">Annuler</button>
-
 </div>
